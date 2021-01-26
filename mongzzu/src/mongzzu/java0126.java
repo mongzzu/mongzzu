c40250cefb6943da37d36f372024688a9371c389
김태현 <enduq111@gmail.com>
2021-01-26 11:40:41 +0900

    0126

--- mongzzu/src/mongzzu/java0126.java
+++ mongzzu/src/mongzzu/java0126.java
@@ -1,36 +1,21 @@
 package mongzzu;
-class Circle{
-	int n;
-	Circle(int n){
-		this.n=n;
+interface Po{
+	void pr(int a,String b);
+	void pr(String b);
+}
+class Point implements Po{
+	public void pr(int a,String b) {
+		System.out.println(a+" "+b);
 	}
-	public String toString() {
-		return n+" ";
+	public void pr(String b) {
+		System.out.println(b);
 	}
-	public boolean equals(Object ob) {
-		if(this.n== ((Circle)ob).n){
-			return true;
-		}
-		return false;
-	}
+}
 class java0126 {
-
 	public void main(String[] args) {
-		Circle a=new Circle(30);
-
-        Circle b=new Circle(30);
-
-        System.out.println("반지름"+a);
-
-        System.out.println("반지름"+b);
-
-        if(a.equals(b)) {
-
-                   System.out.println("같은 원"); }
-
-        else {
-
-                   System.out.println("다른 원");} }
+		Po p=new Point();
+		p.pr(30000,"java");
+		p.pr("python");
 	}
 
 }
